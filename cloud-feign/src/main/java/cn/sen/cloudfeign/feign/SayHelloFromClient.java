dce8e92ed8e44c1510691b270b2858c14b399995
sen.shao <sen.shao@ai-ways.com>
2019-06-05 14:42:37 +0800

    在feign中使用断路器

--- cloud-feign/src/main/java/cn/sen/cloudfeign/feign/SayHelloFromClient.java
+++ cloud-feign/src/main/java/cn/sen/cloudfeign/feign/SayHelloFromClient.java
@@ -1,15 +1,28 @@
 package cn.sen.cloudfeign.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /** 通过feign调用eurake-client端的服务
  * @author shaos
  */
-@FeignClient(value = "eurake-client")
+@FeignClient(value = "eurake-client", fallback = SayHelloFromClient.SayHelloFromClientHystrix.class)
 public interface SayHelloFromClient {
 
     @GetMapping("/hi")
     String helloFromClient(@RequestParam("name") String name);
+
+
+
+    @Component
+    class SayHelloFromClientHystrix implements SayHelloFromClient {
+        @Override
+        public String helloFromClient(String name) {
+            return "sorry " + name;
+        }
+    }
+
+
 }
