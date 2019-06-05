c41f634182b8841fcecffc1f3e90cc1c6bebacf5
sen.shao <sen.shao@ai-ways.com>
2019-06-05 12:11:41 +0800

    rest+ribbon的使用

---
+++ cloud-ribbon/src/main/java/cn/sen/cloudribbon/controller/HelloController.java
@@ -0,0 +1,21 @@
+package cn.sen.cloudribbon.controller;
+
+import cn.sen.cloudribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hello(name);
+    }
+
+}
---
+++ cloud-ribbon/src/main/java/cn/sen/cloudribbon/service/HelloService.java
@@ -0,0 +1,16 @@
+package cn.sen.cloudribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String hello(String name) {
+        return restTemplate.getForObject("http://EURAKE-CLIENT/hi?name=" + name, String.class);
+    }
+}
