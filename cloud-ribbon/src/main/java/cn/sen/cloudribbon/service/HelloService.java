f00e98122e1f867f6ffaf70dac763ef1418f0232
sen.shao <sen.shao@ai-ways.com>
2019-06-05 14:21:14 +0800

    在ribbon中使用hystrix断路器

--- cloud-ribbon/src/main/java/cn/sen/cloudribbon/service/HelloService.java
+++ cloud-ribbon/src/main/java/cn/sen/cloudribbon/service/HelloService.java
@@ -1,5 +1,7 @@
 package cn.sen.cloudribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +12,13 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+
+    @HystrixCommand(fallbackMethod ="hiError")
     public String hello(String name) {
         return restTemplate.getForObject("http://EURAKE-CLIENT/hi?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi " + name + ", sorry error";
     }
 }
