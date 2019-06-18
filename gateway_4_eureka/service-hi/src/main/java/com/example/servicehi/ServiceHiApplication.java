ee45c70632151dfad46f0d5d4cc5cc87f9aa8951
sen.shao <sen.shao@ai-ways.com>
2019-06-18 17:38:43 +0800

    gateway网关配合eureka使用

---
+++ gateway_4_eureka/service-hi/src/main/java/com/example/servicehi/ServiceHiApplication.java
@@ -0,0 +1,29 @@
+package com.example.servicehi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@RestController
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@GetMapping("/hi")
+	public String hi(@RequestParam String name) {
+		return "hi " + name + " from port " + port;
+	}
+}
